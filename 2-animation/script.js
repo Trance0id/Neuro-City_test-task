87abdc7cf19c38c271810e81373b2fb051081f83
Dmitriy Vasnev <trance0id@yandex.ru>
2023-07-03 15:54:19 +0300

    2-animation: finished animation

---
+++ 2-animation/script.js
@@ -0,0 +1,11 @@
+const figures = document.querySelectorAll('.figures__figure');
+const startButton = document.querySelector('.start-button');
+let animationActive = false;
+const animate = () => {
+  figures.forEach(figure => {
+    figure.classList.toggle('animated');
+  });
+  animationActive = !animationActive;
+  startButton.textContent = animationActive ? 'Остановить анимацию' : 'Начать анимацию';
+};
+startButton.addEventListener('click', () => animate());
