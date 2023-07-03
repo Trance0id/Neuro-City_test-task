48105f252dc2190b4c7d4cdddc4a7438c7da7e4f
Dmitriy Vasnev <trance0id@yandex.ru>
2023-07-03 21:56:24 +0300

    5-debounce: finished 5-debounce

---
+++ 5-debounce/script.js
@@ -0,0 +1,23 @@
+const button = document.querySelector('.button');
+const elements = document.querySelectorAll('.elements__element');
+
+function toggleElementsView(e) {
+  elements.forEach(el => el.classList.toggle('hidden'));
+}
+
+function debounce(callback, timeout) {
+  let busy = false;
+  return function (...args) {
+    if (busy) return;
+
+    callback(...args);
+
+    busy = true;
+
+    setTimeout(() => (busy = false), timeout);
+  };
+}
+
+const debouncedToggle = debounce(toggleElementsView, 1000);
+
+button.addEventListener('click', debouncedToggle);
