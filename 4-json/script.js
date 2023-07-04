eb4f7450360a62cde49089b820e9d6be7c6cc5d8
Dmitriy Vasnev <trance0id@yandex.ru>
2023-07-04 07:40:24 +0300

    4-json: added async/await

--- 4-json/script.js
+++ 4-json/script.js
@@ -18,12 +18,43 @@ class Api {
   }
 }
 
-const api = new Api('https://reqres.in/api/users');
-const nav = document.querySelector('.navigation');
-const personList = document.querySelector('.persons');
+function makeSortedArray(arr) {
+  return arr.toSorted((a, b) => {
+    const nameA = a.last_name.toLowerCase();
+    const nameB = b.last_name.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+}
 
-api.getData().then(res => {
-  const numOfPages = res.total_pages;
+async function getPageByNo(pageNo) {
+  try {  
+    const personsUnsorted = await api.getData(pageNo);
+    console.log(personsUnsorted.data);
+    const personsSorted = makeSortedArray(personsUnsorted.data);
+    personsSorted.forEach(person => {
+      const personElement = document.createElement('li');
+      personElement.classList.add('persons__item');
+      Object.keys(person).forEach(key => {
+        const personData = document.createElement('div');
+        personData.textContent = `${key}: ${person[key]};`;
+        personElement.append(personData);
+      });
+      personList.append(personElement);
+    });
+  } catch(err) {
+    console.log(err);
+  }  
+}
+
+async function renderButtons() {
+  const serverData = await api.getData();
+  const numOfPages = serverData.total_pages;
   for (let i = 1; i <= numOfPages; i++) {
     const buttonElement = document.createElement('button');
     buttonElement.setAttribute('type', 'button');
@@ -31,39 +62,29 @@ api.getData().then(res => {
     buttonElement.textContent = String(i);
     buttonElement.addEventListener('click', () => {
       personList.innerHTML = '';
-      getPage(i);
+      getPageByNo(i);
     });
     nav.append(buttonElement);
   }
-});
-
-function getPage(page) {
-  api
-    .getData(page)
-    .then(res => {
-      const persons = res.data.toSorted((a, b) => {
-        const nameA = a.last_name.toLowerCase();
-        const nameB = b.last_name.toLowerCase();
-        if (nameA < nameB) {
-          return -1;
-        }
-        if (nameA > nameB) {
-          return 1;
-        }
-        return 0;
-      });
-      persons.forEach(person => {
-        const personElement = document.createElement('li');
-        personElement.classList.add('persons__item');
-        Object.keys(person).forEach(key => {
-          const personData = document.createElement('div');
-          personData.textContent = `${key}: ${person[key]};`;
-          personElement.append(personData);
-        });
-        personList.append(personElement);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
 }
+
+const api = new Api('https://reqres.in/api/users');
+const nav = document.querySelector('.navigation');
+const personList = document.querySelector('.persons');
+
+renderButtons();
+
+// api.getData().then(res => {
+//   const numOfPages = res.total_pages;
+//   for (let i = 1; i <= numOfPages; i++) {
+//     const buttonElement = document.createElement('button');
+//     buttonElement.setAttribute('type', 'button');
+//     buttonElement.classList.add('navigation__button');
+//     buttonElement.textContent = String(i);
+//     buttonElement.addEventListener('click', () => {
+//       personList.innerHTML = '';
+//       getPageByNo(i);
+//     });
+//     nav.append(buttonElement);
+//   }
+// });
